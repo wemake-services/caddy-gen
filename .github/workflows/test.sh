ac061cdcea8c0bceaef13c47f16274b6965e67d7
Casper da Costa-Luis <casper.dcl@physics.org>
2024-06-11 14:45:15 +0000

    ignore test files

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -11,15 +11,15 @@ curl 127.0.0.1:80
 (( $(docker inspect caddy-gen:latest -f '{{.Size}}') < 100 * 2**20 ))
 
 # test feature CADDY_TEMPLATE
-printf 'http://test-template.localhost {\n  respond "template"\n}\n' > template.tmpl
-docker cp template.tmpl caddy-gen:/tmp/template.tmpl
+printf 'http://test-template.localhost {\n  respond "template"\n}\n' > /tmp/template.tmpl
+docker cp /tmp/template.tmpl caddy-gen:/tmp/template.tmpl
 docker exec caddy-gen sh -c 'export CADDY_TEMPLATE=/tmp/template.tmpl && sh /code/docker-entrypoint.sh'
 sleep 2
 test "$(curl 0.0.0.0:80 -s -H 'Host: test-template.localhost')" = 'template'
 
 # test feature CADDY_SNIPPET
-printf 'http://test-snippet.localhost {\n  respond "snippet"\n}\n' > snippet.tmpl
-docker cp snippet.tmpl caddy-gen:/tmp/snippet.tmpl
+printf 'http://test-snippet.localhost {\n  respond "snippet"\n}\n' > /tmp/snippet.tmpl
+docker cp /tmp/snippet.tmpl caddy-gen:/tmp/snippet.tmpl
 docker exec caddy-gen sh \
   -c 'export CADDY_TEMPLATE=/tmp/template.tmpl CADDY_SNIPPET=/tmp/snippet.tmpl && sh /code/docker-entrypoint.sh'
 sleep 2
